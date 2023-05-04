ab1dc7538d10110ddf96f5bb958af986ae352592
root <root@DESKTOP-PUNVNS3.localdomain>
2023-05-04 14:22:52 +0300

    update

--- sources/Fraction.hpp
+++ sources/Fraction.hpp
@@ -65,7 +65,8 @@ namespace ariel{
             return is;
         }
         
-        int gcd(int num1, int num2);
+        int gcd(int num1, int num2) const;
+        int lcm(int num1, int num2) const;
         Fraction convertToFraction(float num);
         void reduce();
 
